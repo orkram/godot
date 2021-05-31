bfca89aec6746f4717da511a8dfa4f4395b96041
Arkadzi Zaleuski <64438495+orkram@users.noreply.github.com>
2021-05-31 02:18:22 +0200

    Add files via upload
    First game

---
+++ FirstGame/Player.gd
@@ -0,0 +1,56 @@
+extends Area2D
+
+signal hit
+
+var health = 3
+
+export var speed = 400  # How fast the player will move (pixels/sec).
+var screen_size  # Size of the game window.
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	hide()
+
+func _process(delta):
+	var velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+		
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
+
+
+
+func _on_Player_body_entered(_body):
+	if health == 1 :
+		hide() 
+		$CollisionShape2D.set_deferred("disabled", true)
+	emit_signal("hit")
+	
+
+func start(pos):
+	position = pos
+	print_debug('starting...')
+	show()
+	$CollisionShape2D.disabled = false
